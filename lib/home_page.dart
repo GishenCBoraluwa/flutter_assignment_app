4bd718c66b19ea53d281dc11d87c323cad3258fa
Gishen C Boraluwa <bvgcboraluwa@students.nsbm.ac.lk>
2023-07-13 23:32:31 +0530

    home page edits

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,11 +5,13 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar:  AppBar(title:const Text("Home Page"),),
-      body:const Center(
-        child: Text("Hello"),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Page"),
+      ),
+      body: const Center(
+        child: Text("Hello Flutter"),
+      ),
     );
   }
 }
